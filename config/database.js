5b2edf2f13644300ce391525617c1ab6db00be88
MuhammadNasir1 <nmian7080@gmail.com>
2025-03-22 00:04:39 +0500

    fix database connection issue

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: false,  // Add this line
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: false, // Add this line
+    },
+  }
+);
 
 export default sequelize;
