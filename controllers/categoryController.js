97355c72223176a3a33a77cb5232f8750c66aacc
MuhammadNasir1 <nmian7080@gmail.com>
2025-03-23 01:07:04 +0500

    add cache on get categories API

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,7 +2,7 @@ import multer from "multer";
 import path from "path";
 import Category from "../models/categoryModel.js";
 import dotenv from "dotenv";
-
+import NodeCache from "node-cache";
 dotenv.config();
 const BASE_URL = process.env.BASE_URL || "http://localhost:5000";
 // Multer configuration for file upload
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage }).single("category_image");
-
+const cache = new NodeCache({ stdTTL: 60 * 5 }); // Cache for 5 minutes
 export const store = async (req, res) => {
   upload(req, res, async (err) => {
     if (err) {
@@ -45,7 +45,7 @@ export const store = async (req, res) => {
         category_image,
         category_status: category_status || 1, // Default to 1 if not provided
       });
-
+      cache.del("categories");
       return res.status(201).json({
         success: true,
         message: "Category created successfully",
@@ -59,18 +59,35 @@ export const store = async (req, res) => {
 
 export const getAllCategories = async (req, res) => {
   try {
-    const categories = await Category.findAll();
+    // Check if data exists in cache
+    const cachedCategories = cache.get("categories");
+    if (cachedCategories) {
+      return res.status(200).json({
+        success: true,
+        message: "Categories retrieved from cache",
+        data: cachedCategories,
+      });
+    }
 
+    // Fetch from database where category_status = 1
+    const categories = await Category.findAll({
+      where: { category_status: 1 },
+    });
+
+    // Format categories (image path)
     const formattedCategories = categories.map((category) => ({
       category_id: category.category_id,
       category_name: category.category_name,
       category_image: category.category_image
-        ? `${BASE_URL}/uploads/${category.category_image}`
+        ? `${BASE_URL}/uploads/categories/${category.category_image}`
         : null,
       category_status: category.category_status,
       createdAt: category.createdAt,
       updatedAt: category.updatedAt,
     }));
+
+    // Store result in cache
+    cache.set("categories", formattedCategories);
 
     return res.status(200).json({
       success: true,
